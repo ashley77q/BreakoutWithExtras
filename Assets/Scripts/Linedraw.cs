aecfdba537eb3b252ccec56c1b08821a968cf0f7
Ashley <Ashley@tictocgames.com>
2021-03-12 16:00:43 -0800

    update

--- Assets/Scripts/Linedraw.cs
+++ Assets/Scripts/Linedraw.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -22,7 +23,7 @@ public class Linedraw : MonoBehaviour
     //Display distance between two points
     private Text distanceText;
     private float distance;
-      
+    [SerializeField] private Transform _spherePrefab;
 
     public void Start()
     {
@@ -37,46 +38,90 @@ public class Linedraw : MonoBehaviour
         //If mouse is clicked then its the starting point.
         if (Input.GetMouseButtonDown(0))
         {
-
-         startMousePoS = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            
+            startMousePoS = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         }
         if (Input.GetMouseButton(0))
         {
 
-            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-            lineRend.SetPosition(0, new Vector3(startMousePoS.x, startMousePoS.y, 0f));
-            lineRend.SetPosition(1, new Vector3(mousePos.x, mousePos.y, 0f));
-
-            
-
-            distance = (mousePos - startMousePoS).magnitude;
-            distanceText.text = distance.ToString("F2") + "meters";
-
-
-            //transform.position = Vector3.Lerp(mousePos,startMousePoS,changePosition);
-
-
-            transform.position = Vector3.Lerp(startMousePoS, mousePos,changePosition );
-
-
-            if (snapToGrid)
-            {
-                
-
-                transform.position = new Vector3(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.y));
-
-                mousePos = new Vector2(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.y));
-
-                lineRend.SetPosition(1, mousePos);
-            }
-
-
+            DrawLine();
+            DrawDots();
         }
-
-
     }
 
+    private List<Transform> SphereDestroy = new List<Transform>();
+
+    private void DrawDots()
+    {
+        // Destroy Dots from last frame
+        List<Transform> copyOfTheSpheresList = new List<Transform>(SphereDestroy);
+        foreach (var sphere in copyOfTheSpheresList)
+        {
+            SphereDestroy.Remove(sphere);
+            Destroy(sphere.gameObject);
+        }
+
+        //Changes the number of dots that appear on the line
+        //int numberOfSegments = 5;
+        int numberOfSegments = 7;
+            
+
+        Vector3[] points = new Vector3[numberOfSegments + 1];
+        for (int step = 0; step <= numberOfSegments; step++)
+
+        //{
+
+          //  Vector3 dotPosition = points( step * numberOfSegments / distance);
+
+
+       // }
+
+
+        {
+            float percentOfTheLength = (float)step / numberOfSegments;
+            Vector3 positionOfTheDot = Vector3.Lerp(startMousePoS, mousePos, percentOfTheLength);
+            points[step] = positionOfTheDot;
+        }
+        
+
+
+        // At this point, we have all the points.
+        foreach (var dotPosition in points)
+        {
+            Transform sphereInstance = Instantiate(_spherePrefab, dotPosition, Quaternion.identity);
+            SphereDestroy.Add(sphereInstance);
+        }
+    }
+
+
+
+
+    private void DrawLine()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        lineRend.SetPosition(0, new Vector3(startMousePoS.x, startMousePoS.y, 0f));
+        lineRend.SetPosition(1, new Vector3(mousePos.x, mousePos.y, 0f));
+        distance = (mousePos - startMousePoS).magnitude;
+        distanceText.text = distance.ToString("F2") + "meters";
+        transform.position = Vector3.Lerp(mousePos,startMousePoS,changePosition);
+
+        // Originally for moving the sphere at 50%
+        transform.position = Vector3.Lerp(startMousePoS, mousePos, changePosition);
+        if (snapToGrid)
+        {
+            transform.position = new Vector3(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.y));
+            startMousePoS = new Vector3(Mathf.RoundToInt(startMousePoS.x), Mathf.RoundToInt(startMousePoS.y));
+
+
+            // Setting the end point to the sphere transform at 50%
+            mousePos = new Vector2(Mathf.RoundToInt(mousePos.x), Mathf.RoundToInt(mousePos.y));
+            lineRend.SetPosition(1, mousePos);
+            
+        }
+    }
 }
+
+
+
+
+
